3b25d4ff8f3f843ba832b835e142709822d6e440
Lam Nguyen <lamnguyen.sjsu@gmail.com>
2022-08-05 19:42:44 -0700

    CodePath Prework

---
+++ app/src/main/java/com/example/codepath_prework/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.codepath_prework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.helloButton)
+        button.setOnClickListener {
+            Log.v("Hello World!", "Button clicked!")
+            Toast.makeText(this, "Hello, how are you doing?", Toast.LENGTH_LONG).show()
+        }
+    }
+}
